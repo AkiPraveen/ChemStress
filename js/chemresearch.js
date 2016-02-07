5329c7ff51aec138644b6a9029301ef093e453f2
AkiPraveen <akilesh.praveen@gmail.com>
2016-02-06 17:12:12 -0800

    Chem Research Functional
    - Chem Research: fully working for 1 chemical at a time
    - Stoichiometry still needs manual access w/ local copy
    - Removed extra elements on landing page
    - Garbage Cleanup

--- js/chemresearch.js
+++ js/chemresearch.js
@@ -1,3 +1,5 @@
+ // WORKING VERSION FOR ONLY ONE CHEMICAL AT A TIME
+
   var nextURL="";
 
     $(document).ready(function() {
@@ -10,27 +12,37 @@
 
 
         function chemExec(chemName) {
-            var gdata = "";
-            var chemConvention = "";
-
             var query = {
-                url: 'https://search.yahoo.com/search?p=' + chemName + " sds flinnsci safety pdf",
+                url: 'https://www.google.com/search?hl=en&as_q=flinnsci+msds&as_epq=' + chemName + '&as_oq&as_eq&as_nlo&as_nhi&lr&cr&as_qdr=all&as_sitesearch=flinnsci.com&as_occt=any&safe=images&as_filetype=pdf&as_rights',
                 type: 'html',
-                selector: 'span.fz-15px',
+                selector: 'cite',
                 extract: 'text'
-            },
-            uriQuery = encodeURIComponent(JSON.stringify(query)),
-            request  = 'http://example.noodlejs.com/?q=' +
-                     uriQuery + '&callback=?';
+              },
+              uriQuery = encodeURIComponent(JSON.stringify(query)),
+              request  = 'http://example.noodlejs.com/?q=' +
+                         uriQuery + '&callback=?';
 
-//            window.open(query.url);
-            console.log("uriQuery:" + uriQuery);
             // Make Ajax request to Noodle server
             jQuery.getJSON(request, function (data) {
-                gdata = data[0].results + 'JAKUNDO';
-                //confirm(data[0].results[0]);
-                window.open("http://" + data[0].results[0]);                
-            }); 
+              alert(data[0].results);
+
+              var dataRaw = data[0].results;
+
+              var getLink = dataRaw[0];
+
+              
+
+              //window.open("http://" + getLink);
+
+
+              // Find the item with id "flow" and make an <object>
+              document.getElementById("flow").innerHTML = 
+                '<object data="' + 'http://' +  getLink + '" type="application/pdf" style="height:50vh;width:100%;">';
+
+
+            });
+
+
 
 
         }
